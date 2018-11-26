87c67319cc880c8024019f1d69d65a1667e5656c
JasonTy <1026443062@qq.com>
2018-11-26 10:48:30 +0800

    update

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module go/study/demo
+
+require github.com/davecgh/go-spew v1.1.1
--- src/ipc/ipc_test.go
+++ src/ipc/ipc_test.go
@@ -1,8 +1,8 @@
 package ipc
 
 import (
-	"fmt"
 	"testing"
+	"github.com/davecgh/go-spew/spew"
 )
 
 type EchoServer struct {
@@ -26,7 +26,7 @@ func TestIpc(t *testing.T) {
 	resp1, _ := client1.Call("foo", "From Client1")
 	resp2, _ := client2.Call("foo", "From Client2")
 
-	fmt.Println("tangyi:", resp2.Body)
+	spew.Dump("a")
 
 	if 	resp1.Body != "ECHO: foo ~ From Client1" ||
 		resp2.Body != "ECHO: foo ~ From Client2" {
